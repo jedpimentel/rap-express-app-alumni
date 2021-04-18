f448b22c30acc69d1851b8ceaa0dda03798401bf
Jose E. Pimentel <jedpimentel@gmail.com>
2021-04-18 14:24:33 -0400

    create-read-delete first pass

--- public/js/main.js
+++ public/js/main.js
@@ -52,3 +52,43 @@ async function addLike(){
         console.log(err)
     }
 }
+
+async function updateMagic(){
+    // const
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'stageNameS': sName,
+              'birthNameS': bName,
+              'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function deleteMagic(){
+    console.log('magic deletion attempt', this)
+    try{
+        const target = document.querySelector('#magic-delete-target')
+        const response = await fetch('deleteMagic', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'id': target.value
+            })
+          })
+        const data = await response.json()
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
 const PORT = 2121
 require('dotenv').config()
 
@@ -26,10 +27,33 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/',(request, response)=>{
+app.get('/', (request, response) => {
     db.collection('rappers').find().sort({likes: -1}).toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
+app.get('/magic', (request, response) => {
+    console.log('/magic')
+    db.collection('magic').find().toArray()
+    .then(data => {
+        response.render('index.ejs', { magic: data, info: [] })
+    })
+    .catch(error => console.error(error))
+})
+
+// TODO: alert if deletion target doesn't exist
+app.delete('/deleteMagic', (request, response) => {
+    console.log('W00t')
+    console.log(request.body)
+    const targetID = new ObjectID(request.body.id);
+    console.log(targetID)
+    // ObjectId("507c35dd8fada716c89d0013")
+    db.collection('magic').deleteOne({_id: targetID})
+    .then(result => {
+        console.log('Magic Deleted')
+        response.json('Magic Deleted')
     })
     .catch(error => console.error(error))
 })
@@ -48,7 +72,7 @@ app.put('/addOneLike', (request, response) => {
     db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
         $set: {
             likes:request.body.likesS + 1
-          }
+        }
     },{
         sort: {_id: -1},
         upsert: true
@@ -70,6 +94,76 @@ app.delete('/deleteRapper', (request, response) => {
     .catch(error => console.error(error))
 
 })
+
+// app.delete('/deleteMagic', (request, response) => {
+//     console.log(request)
+//     // ObjectId("507c35dd8fada716c89d0013")
+//     db.collection('magic').deleteOne({number: "number"})
+//     .then(result => {
+//         console.log('Magic Deleted')
+//         response.json('Magic Deleted')
+
+//         // response.redirect('/magic')
+//     })
+//     .catch(error => console.error(error))
+// })
+
+// TODO: get initial position from request
+app.post('/initMagicValue', (request, response) => {
+    db.collection('magic')
+    .insertOne({
+        value: 0,
+        object3D: {
+            position: [0, 0, 0],
+            rotation: [0, 0, 0]
+        }
+    })
+    .then(result => {
+        console.log("Magic Position/Rotation reset!");
+        // TODO: UPDATE FRONTEND
+        response.redirect('/magic')
+        // response.json('what does response.json do?')// frontened showed (navigated to?) that text
+    })
+    .catch(error => console.error(error))
+})
+app.put('/updateMagicValue', (request, response) => {
+    ObjectID
+    const new_x = 2;
+    db.collection('magic').updateOne(
+        {_id: 'number', value: 0}
+
+    )
+    .then(result => {
+        response.redirect('/magic')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+        }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.put('/increaseMagicNumber', (request, response) => {
+    db.collection('magic').updateOne({id: 'number'}, {
+        $set: {
+            value: 1
+        }
+    })
+})
+
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
