0ba7506f1a51185c6f2a8a8633bf2cbc2156fb10
Jose E. Pimentel <jedpimentel@gmail.com>
2021-04-21 16:43:37 -0400

    heroku debug

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'rap'
 
-console.log('attempting db connection')
+console.log('attempting db connection', process.env.DB_STRING)
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
